9a40412c885daca803a555eec28d031d7cf7ea30
Sajith Sajeev <sajithsajeevakkalam@gmail.com>
2026-04-17 11:22:22 +0530

    initial commit

---
+++ src/main/java/com/example/sajith/SajithApplication.java
@@ -0,0 +1,12 @@
+package com.example.sajith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SajithApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/sajith/SajithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sajith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
